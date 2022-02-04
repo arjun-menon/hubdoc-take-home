b1ba21c8e7750d78afa455684b4aa71e35343ee1
Arjun G. Menon <arjungmenon@gmail.com>
2022-02-04 09:23:01 -0500

    Delete old SQL code

--- invoice_processor/server.py
+++ invoice_processor/server.py
@@ -1,4 +1,5 @@
 import uvicorn, json, sqlite3
+from threading import Thread, Lock
 from uuid import uuid4
 from time import time
 from processor import extractKeyInformation
@@ -11,23 +12,24 @@ from sqlalchemy.orm import relationship, backref, sessionmaker, joinedload
 verbose = True
 
 engine = create_engine('sqlite://', echo=verbose)
-
 Base = declarative_base()
+dbMutex = Lock()
 
 class ProcessingJob(Base):
     __tablename__ = "ProcessingJob"
     uuid = Column(Text, primary_key=True, nullable=False)
     uploadedBy = Column(Text, nullable=False)
     uploadTimestamp = Column(REAL, nullable=False)
-    filesize = Column(Integer)
+    filesize = Column(Integer, nullable=False)
     vendorName = Column(Text)
     invoiceDate = Column(Text)
     total = Column(REAL)
+    paid = Column (REAL)
     totalDue = Column(REAL)
     currency = Column(Text)
     taxAmount = Column(REAL)
     processingStatus = Column(Text, nullable=False)
-    pdf = Column(BLOB)
+    pdf = Column(BLOB, nullable=False)
 
 Base.metadata.create_all(engine)
 
@@ -39,58 +41,11 @@ def insertPendingJob(uploadedBy, pdf):
     filesize = len(pdf)
     uploadTimestamp = time()
 
-    # self.dbCur.execute('''INSERT INTO ProcessingJob(
-    #     uuid, uploadedBy, uploadTimestamp, filesize, processingStatus, pdf)
-    #     VALUES (?, ?, ?, ?, ?, ?)''', (uuid, uploadedBy, uploadTimestamp, filesize, 'PENDING', pdf))
-
     job = ProcessingJob(uuid=uuid, uploadedBy=uploadedBy, uploadTimestamp=uploadTimestamp,
-        processingStatus='PENDING', pdf=pdf)
-
+        processingStatus='PENDING', filesize=filesize, pdf=pdf)
     session.add(job)
     session.commit()
-
     return uuid
-
-class ProcessingJobs(object):
-    def __init__(self):
-        self.sqlConn = sqlite3.connect(':memory:')
-        self.dbCur = self.sqlConn.cursor()
-
-        self.dbCur.execute(
-            '''CREATE TABLE ProcessingJob(
-                    uuid TEXT PRIMARY KEY NOT NULL,
-                    uploadedBy TEXT NOT NULL,
-                    uploadTimestamp REAL NOT NULL,
-                    filesize INTEGER,
-                    vendorName TEXT,
-                    invoiceDate TEXT,
-                    total REAL,
-                    totalDue REAL,
-                    currency TEXT,
-                    taxAmount REAL,
-                    processingStatus TEXT,
-                    pdf BLOB NOT NULL)''')
-
-    def insertJobRow(self, uuid, uploadedBy, uploadTimestamp, filesize, vendorName, invoiceDate, total, totalDue, currency, taxAmount, processingStatus, pdf):
-        self.dbCur.execute('''INSERT INTO ProcessingJob(
-            uuid, uploadedBy, uploadTimestamp, filesize, vendorName, invoiceDate, total, totalDue, currency, taxAmount, processingStatus, pdf)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-            (uuid, uploadedBy, uploadTimestamp, filesize, vendorName, invoiceDate, total, totalDue, currency, taxAmount, processingStatus, pdf))
-
-    def insertPendingJob(self, uploadedBy, pdf):
-        uuid = uuid4().hex
-        filesize = len(pdf)
-        uploadTimestamp = time()
-        self.dbCur.execute('''INSERT INTO ProcessingJob(
-            uuid, uploadedBy, uploadTimestamp, filesize, processingStatus, pdf)
-            VALUES (?, ?, ?, ?, ?, ?)''', (uuid, uploadedBy, uploadTimestamp, filesize, 'PENDING', pdf))
-        return uuid
-
-    def print(self):
-        for row in self.dbCur.execute('SELECT * FROM ProcessingJob'):
-            print(row)
-
-jobs = ProcessingJobs()
 
 app = Starlette(debug=True)
 
