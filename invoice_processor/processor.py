be8007e6500d082fdfe4464a7c4ba02d1edea5ce
Arjun G. Menon <arjungmenon@gmail.com>
2022-02-04 00:39:59 -0500

    Construct text fragments, ordered appropriately, with stanza/NLP entities.

--- invoice_processor/processor.py
+++ invoice_processor/processor.py
@@ -10,44 +10,79 @@ from pdfminer.pdfpage import PDFPage
 
 class TextFragment(object):
     def __init__(self, textFragment):
-        self.text = self.textFragment
+        self.text = textFragment
+        self.pos = None
+        self.entities = None
+    def __str__(self):
+        return '%s %r' % (self.text, self.entities)
 
 def main():
-    with open("../invoices/HubdocInvoice1.pdf", "rb") as inputFile:
-        minedText1 = mineText1(inputFile)
-        minedText2 = mineText2(inputFile)
+    fragments = constructFragments('../invoices/HubdocInvoice1.pdf')
 
-    fragments = [textFragment for textFragment in 
+    for fr in fragments:
+        print(fr, '\n')
+
+def constructFragments(filename):
+    with open(filename, 'rb') as inputFile:
+        minedTextNormal = mineText1(inputFile)
+        minedTextTagged = mineText2(inputFile)
+
+    fragments = [TextFragment(textFragment) for textFragment in 
             filter(
                 lambda s: len(s) > 0,
                 (map(
                     lambda s: s.strip(), 
-                    minedText1.split('\n'))
+                    minedTextNormal.split('\n'))
                 )
             )
         ]
 
-    # print(fragments)
-    # print('\n\n\n\n')
-    # print(minedText2)
-    # print('\n\n\n\n')
+    for fr in fragments:
+        fr.pos = list(findall(fr.text, minedTextTagged))
+
+    seen = set()
+    for fr in fragments:
+        assert isinstance(fr.pos, list)
+        assert len(fr.pos) > 0
+
+        if len(fr.pos) == 1:
+            fr.pos = fr.pos[0]
+            seen.add(fr.pos)
+        elif len(fr.pos) > 1:
+            for a_pos in fr.pos:
+                (start, end) = a_pos
+                if not isInRanges(start, seen) and not isInRanges(end, seen):
+                    fr.pos = a_pos
+                    seen.add(fr.pos)
+                    break
 
     for fr in fragments:
-        positions = list(findall(fr, minedText2))
-        print(fr, positions)
+        assert isinstance(fr.pos, tuple)
+        start, _ = fr.pos
+        fr.pos = start
+        assert isinstance(fr.pos, int)
 
-    # nlp = stanza.Pipeline(lang='en', processors='tokenize,ner', verbose=False)
-    # for frag in fragments:
-    #     doc = nlp(frag)
-    #     print(frag, repr(doc.ents))
-    #     print()
+    fragments.sort(key=lambda fr: fr.pos)
 
-def findall(p, s): # from https://stackoverflow.com/a/34445090
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,ner', verbose=False)
+    for fr in fragments:
+        doc = nlp(fr.text)
+        fr.entities = doc.ents
+
+    return fragments
+
+def isInRanges(n, ranges):
+        for (start, end) in ranges:
+            if n >= start and n <= end:
+                return True
+        return False
+
+def findall(p, s): # based on https://stackoverflow.com/a/34445090
     '''Yields all the positions of
     the string p in the string s.'''
     i = s.find(p)
     while i != -1:
-        yield i
+        yield (i, i + len(p) - 1)
         i = s.find(p, i+1)
 
 def mineText1(inputFile):
